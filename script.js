69971377172a23da15cd18444d7baea69d914a3a
Joshua Miles <61899128+JoshuaMilesSD@users.noreply.github.com>
2024-06-14 00:00:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 
+ <span class="stat"><strong><span id="xpText">XP: 0</span></strong></span>
+        <span class="stat"><strong><span id="healthText">Health: 100<span><strong><span>
+        <span class="stat"><strong><span id="goldText">Gold: 50</span></strong></span>
